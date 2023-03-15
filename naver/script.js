834ae32a82a7528d66f83939d63087a3e3317e4b
CampbellBiology <urmapw@naver.com>
2023-03-15 11:47:41 +0900

    0314

---
+++ naver/script.js
@@ -0,0 +1,17 @@
+// function log(str){
+//     // let div = document.createElement("div");
+//     let plusTXT = document.querySelector('#plus_txt');
+// //     plusTXT.innerText = str;
+//     // document.querySelector(".log").appendChild(div);
+//   }
+  
+// //   let blurParent = document.querySelector('#form_ID');
+// let blurChild = document.querySelector('#ID');
+
+// // blurParent.addEventListener("blur", e => {
+// //     log("parent blur");
+// //   });
+  
+//   blurChild.addEventListener("blur", e => {
+//     log("추가된 텍스트");
+//   });
